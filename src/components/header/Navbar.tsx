9c0b1d2f94897acaace3480da846a0dad7d34f52
alexisBltz <almamanima@unsa.edu.pe>
2024-09-24 18:27:45 -0500

    Arreglando modificaciones para el navbar responsive

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 
-                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-dojo-800 hover:bg-dojo-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                             <span className="absolute -inset-0.5" />
                             <span className="sr-only">Abrir menu</span>
                             <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
@@ -78,6 +78,8 @@ export default function Navbar() {
                             key={item.text}
                             as="a"
                             href={item.path}
+                            className="text-dojo-700 hover:bg-dojo-900
+                            hover:text-white block rounded-md px-3 py-2 text-base font-medium"
                         >
                             {item.text}
                         </Link>
