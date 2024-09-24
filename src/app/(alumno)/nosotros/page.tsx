3ba6b044e48e78b4b73a281ce46764b5d44f5c2f
alexisBltz <almamanima@unsa.edu.pe>
2024-09-24 18:06:35 -0500

    Modularizando posible paginas

---
+++ src/app/(alumno)/nosotros/page.tsx
@@ -0,0 +1,8 @@
+export default function NosotrosPage() {
+    return (
+        <>
+            <h1>Este es el about</h1>
+            <p>Aqui iría el login si tuviera uno</p>
+        </>
+    )
+}
---
+++ src/app/(alumno)/tareas/page.tsx
@@ -0,0 +1,8 @@
+export default function CursosPage() {
+    return (
+        <>
+            <h1>Este es el tareas</h1>
+            <p>Aqui iría el login si tuviera uno</p>
+        </>
+    )
+}
