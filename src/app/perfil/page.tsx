b02465864cc39fbafa27ef1019c853dd71e1c5c1
alexisBltz <almamanima@unsa.edu.pe>
2024-09-29 08:55:23 -0500

    Creando componentes del header

---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function PerfilPage() {
+    return (
+        <>
+            <h1>Este es perfil</h1>
+            <p>Aqui iría la configuracion si tuviera uno</p>
+        </>
+    )
+}
