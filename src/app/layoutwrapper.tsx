2c46d3b609796315e9f1366d467b012384affc73
Joel <jchinop@unsa.edu.pe>
2024-10-01 02:15:08 -0500

    padding div background

--- src/app/layoutwrapper.tsx
+++ src/app/layoutwrapper.tsx
@@ -18,7 +18,7 @@ function LayoutWrapper({ children }: LayoutWrapperProps) {
     <>
       {showHeaderFooter && <Navbar />}
       <main className="bg-dojo-triangle flex justify-center md:py-6 xl:py-8">
-        <div className="py-2 md:py-0 backdrop-blur-sm xl:backdrop-blur bg-white/5 md:bg-white/40 xl:rounded-md md:w-11/12 2xl:w-5/6">
+        <div className="backdrop-blur-sm xl:backdrop-blur bg-white/5 md:bg-white/40 xl:rounded-md md:w-11/12 2xl:w-5/6 p-2 md:p-3 xl:p-4 ">
           {children}
         </div>
       </main>
